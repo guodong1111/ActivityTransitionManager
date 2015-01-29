24e7dea88f59f4f8dcdbc0e6c12a77b55fe2be0a
guodong1111 <tonytony771111@gmail.com>
2015-01-29 14:37:04 +0800

    make demo app more clean

--- app/src/main/java/tw/guodong/activitytransitionmanager/demo/MainActivity.java
+++ app/src/main/java/tw/guodong/activitytransitionmanager/demo/MainActivity.java
@@ -22,17 +22,22 @@ public class MainActivity extends ActionBarActivity {
         view = findViewById(R.id.textView);
         button = (Button) findViewById(R.id.button);
         imageView = findViewById(R.id.imageView);
-        imageView.setAlpha(0);
         imageView.animate().setDuration(500);
         view.animate().setDuration(1000);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ActivityTransitionManager.getInstance(MainActivity.this).addFormerView(imageView, view);
-                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
                 startActivityForResult(intent, 0);
             }
         });
+    }
+
+    @Override
+    protected void onDestroy() {
+        ActivityTransitionManager.getInstance(this).stopAllAnimation();
+        super.onDestroy();
     }
 
     @Override
